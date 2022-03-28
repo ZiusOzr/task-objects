f2f872eaea20af15fe40e427904516da665e0e36
Zius <lisasmaile3@gmail.com>
2022-03-28 17:44:02 +0300

    задание. объекты

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,12 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data['gender'] == 'female') {
+        delete data.age;
+    } else {
+        if (typeof data['income'] == 'undefined') data.income = 100000;
+    }
+    return data;
 }
 
 /*
@@ -15,6 +21,12 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    let arr = [
+        ...Object.keys(obj1),
+        ...Object.keys(obj2),
+        ...Object.keys(obj3),
+    ];
+    return arr.sort();
 }
 
 /*
@@ -23,4 +35,11 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let arr = [];
+    for (let i = 0; i < count; i++) {
+        let newObj = JSON.parse(JSON.stringify(obj));
+        newObj.id = i;
+        arr.push(newObj);
+    }
+    return arr;
 }
